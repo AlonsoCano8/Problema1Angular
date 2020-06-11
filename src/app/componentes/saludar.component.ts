3f86d27162cd87d5c55ad851cba9bb04ff03855e
Alonso <canoalonso3@gmail.com>
2020-06-11 16:47:42 -0500

    HTML, CSS, TS de un Componente

---
+++ src/app/componentes/saludar.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component (
+    {
+        selector: 'saludar-app',
+        templateUrl: './saludar.component.html',
+        styleUrls: ['./saludar.component.css'] 
+    }
+)
+
+export class SaludarComponent{
+
+}
